67d6d443324a1c8c7dd5dfe9fcbdabd9bf8181f8
ValentinaPanic <valentina.panic87@gmail.com>
2020-12-19 18:03:59 -0700

    fetch(get&post) 4 cocktails

--- src/index.js
+++ src/index.js
@@ -1,44 +1,120 @@
 const cocktailsURL = "http://localhost:3000/cocktails"
-const cocktailDiv = document.getElementById("cocktail-id")
-const cocktailName = document.getElementById("cocktail-name")
-const ingredientUl = document.getElementById("ingredient-list")
-const cocktailImage = document.getElementById("cocktail-image")
-const cocktailInstrcts = document.getElementById("instructions")
+const ingredientsURL = "http://localhost:3000/ingredients"
+const cocktailForm = document.getElementById("cocktail-form")
+const imageInput = document.getElementById("input-image-url")
+const cocktailNameInput = document.getElementById("input-cocktail-name")
+const instructionsInput = document.getElementById("input-instructions")
 
 
+
+cocktailForm.addEventListener("submit", (e) => {submitCocktail(e)})
+
+
+function submitCocktail(e){
+    e.preventDefault()
+   fetch(cocktailsURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+           image: imageInput.value,
+           name: cocktailNameInput.value,
+           instructions: instructionsInput.value
+           
+        })
+    })
+   
+     renderCocktail(cocktailNameInput.value,imageInput.value, instructionsInput.value)
+}
+
+ function renderCocktail(name, image, instructions){
+    //  console.log(name, image, instructions)
+    const cocktailList = document.getElementById("cocktail-list")
+   
+    const cocktailMarkup =`
+    
+            <h3 id="cocktail-name">${name}</h3><br>
+            <img src=${image} id="cocktail-image" width="250" height="250"><br>
+        
+            <label type="text">Instructions:</label>
+            <p id="instructions">${instructions}</p><br>
+        `
+    
+    const ingredientForm = document.createElement('form')
+    ingredientForm.innerHTML += `<input type="text" id="ingredient-input" placeholder ="Ingredient">
+    <input type="submit" value="Add">`
+
+    
+     ingredientForm.addEventListener("submit", (e) => renderIngredient(e))
+
+    const ingrDiv = document.getElementById("ingredient-list")
+    const ingredientList = document.createElement("ul")
+   
+    cocktailList.innerHTML += cocktailMarkup
+    cocktailList.append( ingredientForm, ingredientList)
+    cocktailForm.reset()
+ 
+ }
+
+function renderIngredient(e){
+
+    e.preventDefault()
+    let li = document.createElement('li')
+    let ingredientList = e.target.nextElementSibling
+    let ingredientName = e.target.children[0].value
+    li.innerText = ingredientName
+
+    ingredientList.appendChild(li)
+    e.target.reset()
+    submitIngredient(ingredientName)
+}
+
+ function submitIngredient(ingredientName){
+
+    fetch(ingredientsURL,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+           name: ingredientName
+           
+        })
+    })
+        
+    }
+ 
+fetchCocktails()
 function fetchCocktails(){
     fetch(cocktailsURL)
     .then(response => response.json())
-    .then(cocktails => 
+    .then(cocktails =>{ 
         cocktails.data.forEach(cocktail =>{
-            
-            cocktailImage.src = cocktail.attributes.image
-            cocktailName.innerText = cocktail.attributes.name
-        
-            ingredientList(cocktail)
-            cocktailInstrcts.innerText = cocktail.attributes.instructions
- 
-        })
+            const cocktailData = cocktail.attributes
+            // const ingList = cocktailData.ingredients.forEach(ingr => { ingr.name})
+            renderCocktail(cocktailData.name, cocktailData.image, cocktailData.instructions)
+           
+        })}
      )}
 
-     function ingredientList(cocktail){
-        let ingredients = cocktail.attributes.ingredients
+    
+
+    //  function renderIngredient(cocktail){
+    //     let ingredients = cocktail.attributes.ingredients
       
-        return ingredients.map(ingr => {
+    //         return ingredients.map(ingr => {
 
-            let li = document.createElement('li')
-            // let ingredient = ingr.name
-             li.innerText = ingr.name
-             ingredientUl.appendChild(li)
-            
-            })
+    //         let li = document.createElement('li')
+    //         li.innerText = ingr.name
+    //         ingredientUl.appendChild(li)
+    //         submitIngredient()
+    //         })
+    //  }
 
-            // for (i = 0; i < ingredients.length; i++){
-            //     let li = document.createElement('li')
-            //     let ing = ingredients[i].name
-            //     li.innerText = ing
-            //    ingredientUl.appendChild(li)
-            // }
-            
-     }
-fetchCocktails()
+    
+
+    
+
